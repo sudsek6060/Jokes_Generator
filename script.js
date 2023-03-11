45a0c7cfda0ef2769cf1ae7b33d9ff639da8269a
sudsek6060 <sekhar.sudhansu6060@gmail.com>
2023-03-11 16:31:31 +0530

    completed

--- script.js
+++ script.js
@@ -1,10 +1,40 @@
-const render = async function(){
+
+const fetchApi = async function(){
     try {
-        const joke = await fetch('https://v2.jokeapi.dev/joke/Programming?amount=10')
-        const data = await joke.json()
-        console.log(data.jokes);
+        
+        const res = await fetch('https://v2.jokeapi.dev/joke/Programming?type=twopart&amount=10')
+        const data = await res.json();
+        
+        const jokesArr = (data.jokes);
+        
+         const n = Math.floor((Math.random() * 11)); 
+         const jokeNo = jokeSetup(jokesArr[n]);
+        
+        displayJoke(jokeNo);
+         
     } catch (error) {
         console.log(error);
     }
 }
-render()
+
+const jokeSetup = function(data){
+    
+    return{
+    setup: data.setup,
+    delivery: data.delivery
+    }
+}
+
+const displayJoke = function(data){
+    
+    const html = `
+    <div>
+    <p>// Jokes goes here</p>
+    <h4>${data.setup}</h4>
+    <p >${data.delivery}</p>
+  </div>
+    `
+    document.querySelector('#joke').innerHTML = html
+}
+const btn = document.querySelector('.btn')
+btn.addEventListener('click', () => fetchApi())
